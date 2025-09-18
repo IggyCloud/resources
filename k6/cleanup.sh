e1ec5f6b6564a52212689da37565d41f2dfcac1e
Sebastian <contact@iggycloud.com>
2025-09-18 22:59:19 +0200

    dashboard

--- k6/cleanup.sh
+++ k6/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl delete namespace k6-loadtest --ignore-not-found=true
-kubectl delete pv influxdb-pv grafana-pv --ignore-not-found=true
+kubectl delete pv grafana-pv --ignore-not-found=true
 echo "Cleanup complete"
 echo "Press any key to continue..."
 read -n 1 -s
